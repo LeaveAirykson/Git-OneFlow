e0a5c587998d07384f5ee9b4b52a3f7cd63dbf7e
Georg Böte <georg@neuberger-consulting.com>
2019-07-12 11:56:46 +0200

    Remove uninstall script and made install script copying files instead of symlinking

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# shellcheck disable=SC2063,SC2129,SC2181,SC2162
 
 # ==================================
 # COLORS
@@ -6,22 +7,36 @@
 # terminal formats
 bold="\033[1m"
 green="\033[32m"
+yellow="\033[33m"
 normal="\033[0m"
+
 
 SCRIPTPATH=$(dirname "${BASH_SOURCE[0]}")
 SCRIPTPATH=$(realpath "${SCRIPTPATH}")
 BINPATH=/usr/local/bin
+installed=0
 
-echo -e "\n${bold}WebFlow git extension installer${normal}"
-echo "==============================="
+echo -e "\n${yellow}${bold}Git WebFlow installer"
+echo -e "======================${normal}"
 
+# give some information whats going to happen
+echo -e "This installer will place the scripts ${yellow}git-feature${normal},"
+echo -e "${yellow}git-hotfix${normal} and ${yellow}git-release${normal} in the folder ${yellow}/usr/local/bin.${normal}\n"
 
-symlinkCommands() {
-    echo "Symlinking commands:"
-    for command in commands/*; do
-        ln -sfv "$SCRIPTPATH/$command" "$BINPATH"
-    done
-    echo ""
+# make sure user wants to continue
+read -p "Do you want to continue? (Y/n) " askContinue
+
+echo ""
+
+# abort if wished
+if [ ! "$askContinue" == 'Y' ]; then
+    exit 1
+fi
+
+copyFiles() {
+    echo -e "Moving files into ${yellow}/usr/local/bin${normal} folder."
+    cp -v "$SCRIPTPATH"/commands/* "$BINPATH/"
+    installed=1
 }
 
 setGitConfig() {
@@ -31,12 +46,27 @@ setGitConfig() {
     git config --global webflow.prefix.hotfix "hotfix/"
     git config --global webflow.prefix.release "release/"
     git config --global webflow.prefix.version "v"
+    installed=2
 }
 
-if symlinkCommands ; then
-    echo -e "=> ${green}Symlinked webflow commands ($BINPATH).${normal}"
+removeFolder() {
+    echo -e "\n${green}Install finished!${green}"
+    read -p "You can delete this folder now. Proceed? (Y/n) " askRemoveFolder
+
+    if [ "$askRemoveFolder" == 'Y' ]; then
+        rm -rf "$SCRIPTPATH"
+        exit 1
+    fi
+}
+
+if copyFiles ; then
+    echo -e "\n=> ${green}Installed webflow commands ($BINPATH).${normal}"
 fi
 
 if setGitConfig ; then
-    echo -e "=> ${green}Added webflow configs (~/.gitconfig).${normal}"
+    echo -e "=> ${green}Added webflow configs (~/.gitconfig).${normal}\n"
+fi
+
+if [ $installed == "2" ]; then
+    removeFolder
 fi
